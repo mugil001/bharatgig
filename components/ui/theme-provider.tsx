3855fdf5377a37bd3019cd3213a5e0e5e5c59c46
BharatGig Dev <dev@bharatgig.com>
2026-02-21 15:45:25 +0530

    fix: import ThemeProviderProps from next-themes to resolve type error

--- components/ui/theme-provider.tsx
+++ components/ui/theme-provider.tsx
@@ -2,14 +2,7 @@
 
 import * as React from 'react'
 import { ThemeProvider as NextThemesProvider } from 'next-themes'
-
-interface ThemeProviderProps {
-    children: React.ReactNode
-    attribute?: string
-    defaultTheme?: string
-    enableSystem?: boolean
-    disableTransitionOnChange?: boolean
-}
+import type { ThemeProviderProps } from 'next-themes'
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
     return <NextThemesProvider {...props}>{children}</NextThemesProvider>
